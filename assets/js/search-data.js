1b9d509b2c911a3b0474cc3ac6de332250de7617
valentinsix <124298748+valentinsix@users.noreply.github.com>
2025-12-03 06:38:00 +0000

    Deploying to gh-pages from @ valentinsix/valentinsix.github.io@05f18758427d078937879538a114d88e3758d300 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-wrapped-up-my-research-internship-at-cea-list",
+          title: 'I wrapped up my research internship at CEA-LIST!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
